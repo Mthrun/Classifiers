97aad2d4a1afdf3fd335b98d667a12fc5da7c48b
Mthrun <m.thrun@gmx.net>
2018-02-26 15:40:58 +0100

    .

--- R/SupportVectorMachines.R
+++ R/SupportVectorMachines.R
@@ -1,11 +1,11 @@
-SupportVectorMachines=function(TrainData,TrainCls,TestData,method="C-classification",kernel="linear",degree,gamma,coef0){
+SupportVectorMachines=function(TrainData,TrainCls,TestData,method="C-classification",scale=rep(FALSE,ncol(TrainData)),kernel="linear",degree=3,gamma=0.25,coef0=0){
   requireNamespace('e1071')
-  model=e1071::svm(x=TrainData,y=TrainCls,type=type,kernel=kernel)#,degree=degree,gamma=gamma,coef0=coef0)
+  model=e1071::svm(x=TrainData,y=TrainCls,scale=scale,type=method,kernel=kernel,degree=degree,gamma=gamma,coef0=coef0)
   
-  pred_train <-predict(svm_model,TestData)
+  pred_train <-predict(model,TestData)
   
-  pred_train=round(pred_train,0)
+  pred_train=round(as.numeric(pred_train),0)
   
 
-  return(list(Cls=round(pred_train,0),Probabilities=pred_train,Model=model))
+  return(list(Cls=round(pred_train,0),Probabilities=as.numeric(pred_train),Model=model))
 }
